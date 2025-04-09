14c595b07895315238ef825daa4e8e4ed085d8c2
Diana BOGADO <dianadiazogado@gmail.com>
2025-04-09 14:24:35 -0300

    Agregue el archivo scss para darle estilos a la card, agregue los hooks para que cambie de titulo dependiedno la pagina en la que esta, y los items del carrito

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,33 @@
+import './Inicio.scss'
+import Card from "../components/Card";
+import { useContext } from 'react';
+import ProductosContext from '../contexts/ProductosContext';
+import useTitulo from '../hooks/useTitulo';
+
+const Inicio = () => {
+  const { productos } = useContext(ProductosContext)
+
+  useTitulo('Inicio')
+
+  return (
+    <main>
+      <section className="section-cards">
+        <header className="section-cards__header">
+          <h1>Más vendidos de esta semana en tecnologia</h1>
+          <p>Se encontraron 8 productos</p>
+        </header>
+      </section>
+
+      <section className="cards-container" id="container-productos">
+        {
+          productos && productos.map((producto) => (
+            <Card producto={producto} key={producto.id} />
+          ))
+        }
+
+      </section>
+    </main>
+  );
+};
+
+export default Inicio;
