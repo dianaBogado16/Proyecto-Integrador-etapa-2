e2a10dfe6169f1c85963c0556d4a5efc3ee3eb4b
Diana BOGADO <dianadiazogado@gmail.com>
2025-04-08 22:34:04 -0300

    Agregue el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router"
 import Cabecera from "./components/cabecera"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <BrowserRouter>
       
       <Cabecera/>
+
+      <Footer/>
 
     
     </BrowserRouter>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 
 const Footer = () => {
     return (
-      <div>Footer</div>
+        <footer className="foot">
+        <div className="seguir">
+          <p>Seguinos en nuestras redes sociales!</p>
+        </div>
+  
+        <div className="logos">
+          <div className="instagram">
+            <i className="fa-brands fa-instagram fa-2x"></i>
+          </div>
+  
+          <div className="facebook">
+            <i className="fa-brands fa-facebook fa-2x"></i>
+          </div>
+  
+          <div className="twitter">
+            <i className="fa-brands fa-twitter fa-2x"></i>
+          </div>
+        </div>
+        
+  
+        <p className="copy">Copyright 2025©. Designed by TecnoBaires</p>
+      </footer>
     )
   }
   
