cdfcd756d76cfab26f2c4a757bf365b27894d29c
Diana BOGADO <dianadiazogado@gmail.com>
2025-04-10 17:43:29 -0300

    Agregue un h3 donde dice el total de productos del carrito que se va actualizando con el deseo del usuario

--- src/components/ListadoCarrito.jsx
+++ src/components/ListadoCarrito.jsx
@@ -1,63 +1,70 @@
-import { useContext } from "react"
-import CarritoContext from "../contexts/CarritoContext"
-import ItemCarrito from "./ItemCarrito"
-import './ListadoCarrito.scss'
+import { useContext } from "react";
+import CarritoContext from "../contexts/CarritoContext";
+import ItemCarrito from "./ItemCarrito";
+import './ListadoCarrito.scss';
 
 const ListadoCarrito = () => {
-
     const { 
         carrito, 
         limpiarCarritoContext, 
-        guardarCarritoBackendContext } = useContext(CarritoContext)
-    
-    console.log(carrito)
+        guardarCarritoBackendContext 
+    } = useContext(CarritoContext);
+
+    console.log(carrito);
 
     const handleComprar = () => {
-        console.log('Comprando...')
-        guardarCarritoBackendContext()
-    }
-    
+        console.log('Comprando...');
+        guardarCarritoBackendContext();
+    };
+
     const handleLimpiarCarrito = () => {
-        console.log('Vaciando carrito...')
-        limpiarCarritoContext()
-    }
+        console.log('Vaciando carrito...');
+        limpiarCarritoContext();
+    };
 
-  return (
-    <>
-    <table className='tabla-carrito'>
-        <thead>
-            <tr>
-                <th>Foto</th>
-                <th>Nombre</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                carrito.length <= 0 ? (
+    // Función para calcular el total de ítems
+    const calcularTotalItems = () => {
+        return carrito.reduce((total, producto) => total + producto.cantidad, 0);
+    };
+
+    return (
+        <>
+           
+            <table className='tabla-carrito'>
+                <thead>
                     <tr>
-                        <td colSpan={5} style={{textAlign: 'center'}}>No hay productos</td>
+                        <th>Foto</th>
+                        <th>Nombre</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
                     </tr>
-                ) : (
-                    carrito.map((producto, idx) => (
-                        <ItemCarrito key={idx} producto={producto} />
-                    ))
-                )
-            }
-        </tbody>
-    </table>
-    
-    { !carrito.length <= 0 && (
-            <>
-                <button className="btn-vaciar" onClick={handleLimpiarCarrito}>Vaciar Carrito</button>
-                <button className="btn-comprar" onClick={handleComprar}>Comprar</button>
-            </>
-        )
-    }
-</>
-  )
-}
+                </thead>
+                <tbody>
+                    {
+                        carrito.length <= 0 ? (
+                            <tr>
+                                <td colSpan={5} style={{ textAlign: 'center' }}>No hay productos</td>
+                            </tr>
+                        ) : (
+                            carrito.map((producto, idx) => (
+                                <ItemCarrito key={idx} producto={producto} />
+                            ))
+                        )
+                    }
+                </tbody>
+            </table>
+             <h3 className="total-items">Total de ítems: {calcularTotalItems()}</h3>
+            
+            
+            { !carrito.length <= 0 && (
+                <>
+                    <button className="btn-vaciar" onClick={handleLimpiarCarrito}>Vaciar Carrito</button>
+                    <button className="btn-comprar" onClick={handleComprar}>Comprar</button>
+                </>
+            )}
+        </>
+    );
+};
 
-export default ListadoCarrito
+export default ListadoCarrito;
